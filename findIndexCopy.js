07af5de76a47e8cbb167f84a2a1dbf9189befe0a
rafgrig <150832432+rafgrig@users.noreply.github.com>
2024-09-04 19:21:07 +0400

    Add files via upload

---
+++ findIndexCopy.js
@@ -0,0 +1,15 @@
+const numbers = [1,2,3,4,5]
+
+function MyFingIndex(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i, array)) {
+          return i;
+      }
+  }
+
+  return undefined
+}
+
+const findIndex = MyFingIndex(numbers, element => element > 2)
+
+console.log(findIndex)
